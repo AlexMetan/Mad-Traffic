74634f0e835f6ac602bb4ae1356a8d5b3fae84fd
Alex Metan <dudkaoleksandr98@gmail.com>
2019-10-09 16:05:49 +0200

    tree,pp

---
+++ Assets/script/StartGame.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+    [SerializeField] Transform mainCamera;
+    [SerializeField] Vector3 cameraStartPosition;
+    [SerializeField] Vector3 cameraDefPosition;
+    [SerializeField] GameObject trafficSpawner;
+    [SerializeField] GameObject mainUI;
+    [SerializeField] GameObject menuUI;
+    void Start()
+    {
+        CameraPositionOnStart();
+        TrafficSpawnerSetActive(false);
+        MainUISetActive(false);
+        TrafficSpawnerSetActive(false);
+    }
+    void MainUISetActive(bool value)
+    {
+        mainUI.SetActive(value);
+        menuUI.SetActive(!value);
+
+    }
+    void CameraPositionOnStart()
+    {
+        mainCamera.position=cameraStartPosition;
+    }
+    void TrafficSpawnerSetActive(bool value)
+    {
+        trafficSpawner.SetActive(value);
+    }
+    public void StartMainGame()
+    {
+        StartCoroutine(MoveCameraToDef());
+        
+    }
+    IEnumerator MoveCameraToDef()
+    {
+        while (mainCamera.position!=cameraDefPosition)
+        {
+            mainCamera.position=Vector3.MoveTowards(mainCamera.position,cameraDefPosition,1);
+            yield return null;
+        }
+        TrafficSpawnerSetActive(true);
+        MainUISetActive(true);
+        yield break;
+    }
+}
